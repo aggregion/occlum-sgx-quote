707665fbbfcf5c6ecadce1e0f3fb3d8aa1e8a61c
Ivan Chirkin <chirkin.ivan@gmail.com>
2023-06-03 08:18:27 +0300

    improve example

--- examples/quote.rs
+++ examples/quote.rs
@@ -4,13 +4,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let buf = SGXQuoteBuilder::generate_bytes("test data")?;
     let quote = SGXQuote::try_from(&buf)?;
 
-    println!("{:#?}", quote);
-
     if quote.verify()? {
         println!("Quote is valid");
     } else {
         println!("Quote is invalid");
     }
+
+    let _mrenclave = quote.mrenclave();
+    let _mrsigner = quote.mrsigner();
+    let _product_id = quote.product_id();
+    let _version = quote.version();
+
+    println!("{:#?}", quote);
 
     Ok(())
 }
